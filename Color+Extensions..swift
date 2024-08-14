3e1907c21569d61586ad10268abee461f4d121be
KayqueDosAnjos <kayquesvieiras@icloud.com>
2024-08-14 16:54:32 -0300

    Initial Commit

---
+++ Color+Extensions..swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extensions..swift
+//  WeatherApp2
+//
+//  Created by Kayque Dos anjos on 14/08/24.
+//
+
+import Foundation
---
+++ WeatherApp2/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  Weather App
+//
+//  Created by KayqueDosAnjos on 02/08/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let contrastColor = UIColor(named: "contrastColor")
+    static let primaryColor = UIColor(named: "primaryColor")
+    static let SoftGray = UIColor(named: "softGray")
+}
